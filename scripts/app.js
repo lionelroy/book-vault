7871bb0f57b0f4fe7473b13ee1f9bce46c593ec8
Stephane Roy <stephroy411@hotmail.com>
2021-08-27 13:02:20 -0300

    Add function addBookToLibrary

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+const tbody = document.querySelector('tbody');
+const addNewBook = document.querySelector('#add');
+const form = document.querySelector('#form');
+const formValid = document.querySelector('#form-validation');
+
+let myLibrary = [];
+
 class Book {
   constructor(id, title, author, pages, read) {
     this.id = id;
@@ -7,3 +14,30 @@ class Book {
     this.read = read;
   }
 }
+
+function addBookToLibrary() {
+  // generate an id for a new book
+  const id = myLibrary.length === 0 ? 1 : myLibrary[myLibrary.length - 1].id + 1;
+  // Capture form fields
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
+  // ensure that form fields is not empty
+  if (author === '' || title === '' || pages === '' || pages == 0) {
+      formValid.style.display = 'block'
+      setTimeout(() => {
+          formValid.style.display = 'none'
+      }, 3000)
+      return;
+  }
+  // create new instance from Book constructor
+  const book = new Book(id, title, author, pages, read)
+  // clear form fields
+  form.reset();
+  myLibrary.push(book)
+  // Save myLibrary array in the browser local storage
+  localStorage.setItem('myLib', JSON.stringify(myLibrary))
+   displayBooks()
+}
+
